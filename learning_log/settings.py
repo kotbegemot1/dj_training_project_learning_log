6fff1bf68efbc67178423c2469f10455a7e31609
kotbegemot1 <kk1slorod@gmail.com>
2020-02-10 15:24:20 +0300

    Ready for deployment to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -131,7 +131,7 @@ if cwd == '/app' or cwd[:4] =='/tmp':
 
     #конфигурация статистических ресурсов
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = 'static'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static')
     )
